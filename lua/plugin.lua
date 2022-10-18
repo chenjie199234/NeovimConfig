0173b7e64a39f48c7ade1919deec94e0878768f3
nobody <lilithgames@lilithgamesdeMacBook-Pro-3.local>
2022-10-18 11:26:53 +0800

    update

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -29,6 +29,7 @@ require('packer').startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
   use 'hrsh7th/nvim-cmp'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
--- lua/plugin_lsp.lua
+++ lua/plugin_lsp.lua
@@ -87,18 +87,21 @@ cmp.setup({
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' },
+		{ name = 'nvim_lsp_signature_help' }
 	}, {
-		{ name = 'buffer' },
+		{ name = 'buffer' }
 	})
 })
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
 		{ name = 'buffer' }
 	}
 })
 cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-        	{ name = 'path' }
+		{ name = 'path' }
 	}, {
 		{ name = 'cmdline' }
 	})
