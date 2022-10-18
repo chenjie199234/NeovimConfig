6a3e3987ccde6fe48c3df109be0103c8043ec977
nobody <lilithgames@lilithgamesdeMacBook-Pro-3.local>
2022-10-18 11:18:07 +0800

    update

--- lua/plugin_lsp.lua
+++ lua/plugin_lsp.lua
@@ -108,6 +108,6 @@ cmp.setup.cmdline(':', {
 require("luasnip.loaders.from_vscode").lazy_load()
 
 --start lsp server
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig')['gopls'].setup {capabilities = capabilities}
 require('lspconfig')['clangd'].setup {capabilities = capabilities}
